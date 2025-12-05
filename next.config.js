3ba1ca448c67dc37316a81cd40ce677c879cb9ba
kalidasan00 <kalidas4446@gmail.com>
2025-12-05 20:25:18 +0530

    Updated Next.js config for dynamic server deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-/** next.config.js */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    // allow local images from /public
-    unoptimized: true
-  }
+  output: 'standalone', // required for Vercel server functions
+  experimental: {
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
